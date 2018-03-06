e672aa35ff7ed7fe534abdb61eb3bc5e45da43a2
Kayne <kayne@kayne.com>
2018-03-05 20:49:00 -0600

    Updated lab 4

--- Lab 4 Finished.js
+++ Lab 4 Finished.js
@@ -1,5 +1,5 @@
 /*Part A*/
-    var base = EUR : 1
+    var base = EUR : 1;
     var rates = {
       "AUD": 1.5417,
       "BGN": 1.9558,
@@ -10,18 +10,12 @@
     rates.CHF = 1.1787;
 
     rates.OneHundredEUR() {
-      TotalEUR = EUR * 100
-      AUDexchange = TotalEUR * AUD
-      console.log('Australian Dollars Exhange for 100 Euros is ' + AUDexchange)
+      TotalEUR = EUR * 100;
+      AUDexchange = TotalEUR * AUD;
+      console.log("Australian Dollars Exhange for 100 Euros is " + AUDexchange)
     };
 
-    rates.HighestExchange() {
-      for (var rate in rates) {
-        if (rate > EUR) {
-          console.log('The ' + rate + 'is the highest exchange rate.')
-        }
-      }
-    };
+    rates.Max();
 
 /*PArt B*/
 
@@ -49,4 +43,146 @@ if(name = 'Gary Oldman'){
   console.log(cats_and_owners.cat[1])
 };
 
-cats_and_owners.
+cats_and_owners.push({name : 'Taylor Swirft', cat : 'Meredith'});
+
+for (var OaC in cats_and_owners) {
+  console.log(Oac);
+  console.log(cats_and_owners[OaC]);
+};
+
+/*Part D*/
+
+var nobel_prize_winners_2017 = {
+  "prizes":[
+    {
+      "year":"2017",
+      "category":"physics",
+      "laureates":[
+        {
+          "id":"941",
+          "firstname":"Rainer",
+          "surname":"Weiss",
+          "motivation":"\"for decisive contributions to the LIGO detector and the observation of gravitational waves\"",
+          "share":"2"
+        },
+        {
+          "id":"942",
+          "firstname":"Barry C.",
+          "surname":"Barish",
+          "motivation":"\"for decisive contributions to the LIGO detector and the observation of gravitational waves\"",
+          "share":"4"
+        },
+        {
+          "id":"943",
+          "firstname":"Kip S.",
+          "surname":"Thorne",
+          "motivation":"\"for decisive contributions to the LIGO detector and the observation of gravitational waves\"",
+          "share":"4"
+        }
+      ]
+    },
+    {
+      "year":"2017",
+      "category":"chemistry",
+      "laureates":[
+        {
+          "id":"944",
+          "firstname":"Jacques",
+          "surname":"Dubochet",
+          "motivation":"\"for developing cryo-electron microscopy for the high-resolution structure determination of biomolecules in solution\"",
+          "share":"3"
+        },
+        {
+          "id":"945",
+          "firstname":"Joachim",
+          "surname":"Frank",
+          "motivation":"\"for developing cryo-electron microscopy for the high-resolution structure determination of biomolecules in solution\"",
+          "share":"3"
+        },
+        {
+          "id":"946",
+          "firstname":"Richard",
+          "surname":"Henderson",
+          "motivation":"\"for developing cryo-electron microscopy for the high-resolution structure determination of biomolecules in solution\"",
+          "share":"3"
+        }
+      ]
+    },
+    {
+      "year":"2017",
+      "category":"medicine",
+      "laureates":[
+        {
+          "id":"938",
+          "firstname":"Jeffrey C.",
+          "surname":"Hall",
+          "motivation":"\"for their discoveries of molecular mechanisms controlling the circadian rhythm\"",
+          "share":"3"
+        },
+        {
+          "id":"939",
+          "firstname":"Michael",
+          "surname":"Rosbash",
+          "motivation":"\"for their discoveries of molecular mechanisms controlling the circadian rhythm\"",
+          "share":"3"
+        },
+        {
+          "id":"940",
+          "firstname":"Michael W.",
+          "surname":"Young",
+          "motivation":"\"for their discoveries of molecular mechanisms controlling the circadian rhythm\"",
+          "share":"3"
+        }
+      ]
+    },
+    {
+      "year":"2017",
+      "category":"literature",
+      "laureates":[
+        {
+          "id":"947",
+          "firstname":"Kazuo",
+          "surname":"Ishiguro",
+          "motivation":"\"who, in novels of great emotional force, has uncovered the abyss beneath our illusory sense of connection with the world\"",
+          "share":"1"
+        }
+      ]
+    },
+    {
+      "year":"2017",
+      "category":"peace",
+      "laureates":[
+        {
+          "id":"948",
+          "firstname":"International Campaign to Abolish Nuclear Weapons (ICAN)",
+          "motivation":"\"for its work to draw attention to the catastrophic humanitarian consequences of any use of nuclear weapons and for its ground-breaking efforts to achieve a treaty-based prohibition of such weapons\"",
+          "share":"1",
+          "surname":""
+        }
+      ]
+    },
+    {
+      "year":"2017",
+      "category":"economics",
+      "laureates":[
+        {
+          "id":"949",
+          "firstname":"Richard H.",
+          "surname":"Thaler",
+          "motivation":"\"for his contributions to behavioural economics\"",
+          "share":"1"
+        }
+      ]
+    }
+  ]
+};
+var physics_laureates = nobel_prize_winners_2017.prizes[0].laureates;
+console.log(physics_laureates)
+
+var prizes = nobel_prize_winners_2017.prizes
+
+console.log(prizes.length)
+
+for (var i = 0 ; i < prizes.length ; i++) {
+  console.log(prizes[i])
+}
